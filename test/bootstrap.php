a76638c66ba87bd8888358ccada3b32ac335fabd
ASCII Soup <nils.luxton@gmail.com>
2013-07-09 13:33:37 +0100

    Fixed namespace regression + 5.3 compatibility in tests

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -4,3 +4,4 @@ define('FIXTURE_DIR', __DIR__ . '/fixture');
 
 require dirname(__DIR__) . '/autoload.php';
 require __DIR__ . '/fixture/ProviderTestClasses.php';
+require __DIR__ . '/fixture/NamespacedClass.php';
