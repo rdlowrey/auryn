875b05cf194f54155986de6333f5b6dce12ad9ad
Daniel Lowrey <rdlowrey@gmail.com>
2013-11-30 12:50:44 -0500

    Test and dir structure changes

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -2,6 +2,6 @@
 
 define('FIXTURE_DIR', __DIR__ . '/fixture');
 
-require dirname(__DIR__) . '/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/fixture/ProviderTestClasses.php';
 require __DIR__ . '/fixture/NamespacedClass.php';
