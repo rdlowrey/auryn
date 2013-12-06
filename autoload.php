db3c74148539cdae1ccce59aeee843d014e1f951
Daniel Lowrey <rdlowrey@gmail.com>
2013-12-06 10:53:26 -0500

    Reverted autoloader changes that conflict with composer

--- autoload.php
+++ autoload.php
@@ -3,6 +3,6 @@
 spl_autoload_register(function($class) {
     if (0 === strpos($class, 'Auryn\\')) {
         $normalizedClass = str_replace('\\', '/', $class);
-        require __DIR__ . "/../lib/{$normalizedClass}.php";
+        require __DIR__ . "/lib/{$normalizedClass}.php";
     }
 });
